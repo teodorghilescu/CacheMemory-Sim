5ff920702b4c9bb4ebcf32900fad5ac8d19b1c5e
Ghilescu Teodor-Andrei <33061287+teodorghilescu@users.noreply.github.com>
2019-02-17 18:30:26 +0200

    Add files via upload

---
+++ Cache.java
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * @author Teodor
+ *	Cache interface which is being implemented by the three methods
+ */
+public interface Cache {
+	
+	public void remove(Premium holder);
+	public int add(Premium holder);
+}
